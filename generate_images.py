b9783ae9ab77d44a9eb1846df3d9e30d1f8beae9
Sommer <118508483+Sommervt@users.noreply.github.com>
2025-08-16 00:30:01 -0600

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -103,7 +103,7 @@ async def main() -> None:
     if not access_token:
         # access_token = os.getenv("GITHUB_TOKEN")
         raise Exception("A personal access token is required to proceed!")
-    user = os.getenv("Sommervt")
+    user = os.getenv"Sommervt"
     if user is None:
         raise RuntimeError("Environment variable GITHUB_ACTOR must be set.")
     exclude_repos = os.getenv("EXCLUDED")
@@ -134,4 +134,5 @@ async def main() -> None:
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
